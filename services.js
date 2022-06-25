91dd0e699f6c53d81b560f2721453bec233a15c2
franco <esquisino2@hotmail.com>
2022-06-25 20:13:53 -0300

    se modifico estilos

--- services.js
+++ services.js
@@ -1,3 +1,7 @@
+let imagePokemon = document.getElementById("imagePokemon");
+let namePokemon = document.getElementById("namePokemon");
+let abilityPokemon = document.getElementById("abilityPokemon");
+
 export function showPokemons() {
   fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20")
     .then((res) => res.json())
@@ -9,7 +13,8 @@ function viewListPokemon(pokemon) {
   boards.innerHTML = "";
   pokemon.results.forEach((poke) => {
     const $link = document.createElement("a");
-    $link.classList = "list-group-item list-group-item-action";
+    $link.classList =
+      "list-group-item list-group-item-action m-1 border border-primary";
     $link.dataset.toggle = "list";
     $link.setAttribute("href", poke.url);
     $link.textContent = poke.name;
@@ -19,10 +24,24 @@ function viewListPokemon(pokemon) {
 }
 
 function fetchPokemon(e) {
-  listPokemon(e.target.href);
+  fetch(e.target.href)
+    .then((res) => res.json())
+    .then((json) => pokemonCards(json));
   return false;
 }
 
-function listPokemon(e) {
-  alert(e);
+function pokemonCards(pokemon) {
+  abilityPokemon.innerHTML = "";
+  if (pokemon.sprites.other.dream_world.front_default === null) {
+    imagePokemon.src = pokemon.sprites.front_default;
+  } else {
+    imagePokemon.src = pokemon.sprites.other.dream_world.front_default;
+  }
+  namePokemon.innerHTML = `${pokemon.name}`;
+
+  pokemon.abilities.forEach((element) => {
+    let p = document.createElement("p");
+    p.innerHTML = `${element.ability.name}`;
+    abilityPokemon.appendChild(p);
+  });
 }
